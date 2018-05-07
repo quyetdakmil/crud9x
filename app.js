1ada9910f8a308aed938bc0b96b3cd400ae7f970
quyetdakmil <37035949+quyetdakmil@users.noreply.github.com>
2018-05-07 19:56:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/api', celebrities);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');

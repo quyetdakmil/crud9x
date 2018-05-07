bac2eeda2974f211086efd2def84ce4528e8479c
quyetdakmil <37035949+quyetdakmil@users.noreply.github.com>
2018-05-07 20:30:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -34,21 +34,12 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-
-var allowCrossDomain = function(req, res, next) {
-    if ('OPTIONS' == req.method) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.use(allowCrossDomain);
-
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 
 // error handler
 app.use(function(err, req, res, next) {
